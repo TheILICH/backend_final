819f8c9f406f441baf4a7fa02ba74a3646267fad
Iliyas Bekkhozha <iliyasbek2003@gmail.com>
2023-05-13 21:56:21 +0600

    commit

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
 from django.shortcuts import render
+from .forms import MyUserCreationForm
+from django.contrib.auth.forms import UserCreationForm, AuthenticationForm
 
-# Create your views here.
+
+def home(request):
+    return render(request, 'main page.html')
+
+
+def login(request):
+    return render(request, 'login.html')
+
+
+def register(request):
+    form = MyUserCreationForm()
+
+    if request.method == 'POST':
+        form = MyUserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+        else:
+            print('FORM IS NOT VALID')
+
+    content = {
+        'form': form,
+    }
+
+    return render(request, 'register.html', content)
--- blogging_platform/urls.py
+++ blogging_platform/urls.py
@@ -15,7 +15,7 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
-from .views import home, login, register
+from accounts.views import home, login, register
 
 urlpatterns = [
     path('', home),
